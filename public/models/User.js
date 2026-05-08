c04b08de2d52a2debff0ecb2d5dafe18ab9a1ca1
Durup07 <durgakumaridto@gmail.com>
2026-05-08 12:18:46 +0530

    update website

--- public/models/User.js
+++ public/models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  role: String // teacher / student
+
+name: String,
+email: { type: String, unique: true },
+password: String,
+role: { type: String, enum: ["admin","teacher","student"] },
+image: String
+
 });
 
 module.exports = mongoose.model("User", userSchema);
---
+++ public/models/marks.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const marksSchema = new mongoose.Schema({
+studentEmail: String,
+subject: String,
+marks: Number
+});
+
+module.exports = mongoose.model("Marks", marksSchema);
